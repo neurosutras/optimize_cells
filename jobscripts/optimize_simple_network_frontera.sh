7fd88eebea49e7129654c25360b2245e9cb29742
Aaron D. Milstein <neurosutras@gmail.com>
2019-12-13 16:37:19 -0800

    20191213: optimize_simple_network:  - Model J initializes E_E and E_FF connections with lognormal weights.  - Model large_I increases the number of cells 10-fold.  - Benchmarking on TACC Frontera.

--- jobscripts/optimize_simple_network_frontera.sh
+++ jobscripts/optimize_simple_network_frontera.sh
@@ -8,7 +8,7 @@ sbatch <<EOT
 #SBATCH -e /scratch1/06441/aaronmil/src/optimize_cells/logs/"$JOB_NAME".%j.e
 #SBATCH -p normal
 #SBATCH -N 400
-#SBATCH -n 11200
+#SBATCH -n 22400
 #SBATCH -t 6:00:00
 #SBATCH --mail-user=aaronmil@stanford.edu
 #SBATCH --mail-type=BEGIN,END,FAIL
@@ -17,6 +17,6 @@ set -x
 
 cd $SCRATCH/src/optimize_cells
 
-ibrun -n 11200 python3 -m nested.optimize --config-file-path=config/optimize_simple_network_"$1"_config.yaml /
+ibrun -n 22400 python3 -m nested.optimize --config-file-path=config/optimize_simple_network_"$1"_config.yaml /
     --output-dir=data --pop_size=200 --max_iter=50 --path_length=3 --disp --procs_per_worker=112 --export
 EOT
