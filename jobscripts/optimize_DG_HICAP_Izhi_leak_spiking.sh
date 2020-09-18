6b195b9ffc40a67268b78195164f373764f3ac71
Prannath Moolchand <prannath_moolchand@alumni.brown.edu>
2020-09-18 02:52:42 -0500

    20200918 - good objectives for HICAP/IS, jobscripts

--- jobscripts/optimize_DG_HICAP_Izhi_leak_spiking.sh
+++ jobscripts/optimize_DG_HICAP_Izhi_leak_spiking.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -l
 
-#SBATCH -J optimize_DG_BC_Izhi_leak_spiking_cell1000000
-#SBATCH -o /scratch1/04119/pmoolcha/HDM/optimize_cells/logs/optimize_DG_BC_Izhi_leak_spike_cell1039000_20200717.%j.o
-#SBATCH -e /scratch1/04119/pmoolcha/HDM/optimize_cells/logs/optimize_DG_BC_Izhi_leak_spike_cell1039000_20200717.%j.e
+#SBATCH -J optimize_DG_HICAP_Izhi_leak_spiking_cell1000000
+#SBATCH -o /scratch1/04119/pmoolcha/HDM/optimize_cells/logs/optimize_DG_HICAP_Izhi_leak_spike_cell1043250_20200724.%j.o
+#SBATCH -e /scratch1/04119/pmoolcha/HDM/optimize_cells/logs/optimize_DG_HICAP_Izhi_leak_spike_cell1043250_20200724.%j.e
 #SBATCH -p normal 
 #SBATCH -N 22 -n 1232 
 #SBATCH -t 0:30:00
@@ -18,4 +18,4 @@ cd /scratch1/04119/pmoolcha/HDM/optimize_cells
 #ibrun gdb --batch -x /scratch1/04119/pmoolcha/HDM/optimize_cells/gdb_script --args python3 -m nested.optimize --config-file-path='config/optimize_DG_MC_excitability_config.yaml' --pop_size=200 --max_iter=2 --path_length=1 --disp --output-dir=data --label=cell1000000 --export
  
 
-ibrun python3 -m nested.optimize --config-file-path='config/optimize_DG_BC_Izhi_leak_spiking_config.yaml' --pop_size=200 --max_iter=50 --path_length=3 --disp --output-dir=data --label=cell1039000
+ibrun python3 -m nested.optimize --config-file-path='config/optimize_DG_HICAP_Izhi_leak_spiking_config.yaml' --pop_size=200 --max_iter=50 --path_length=3 --disp --output-dir=data --label=cell1043250
--- jobscripts/optimize_DG_MC_Izhi_leak_spiking.sh
+++ jobscripts/optimize_DG_MC_Izhi_leak_spiking.sh
@@ -5,7 +5,7 @@
 #SBATCH -e /scratch1/04119/pmoolcha/HDM/optimize_cells/logs/optimize_DG_MC_Izhi_leak_spike_cell1000000_20200612.%j.e
 #SBATCH -p normal 
 #SBATCH -N 22 -n 1232 
-#SBATCH -t 3:00:00
+#SBATCH -t 1:00:00
 #SBATCH -A BIR20001
 #SBATCH --mail-user=pmoolcha@stanford.edu
 #SBATCH --mail-type=BEGIN,END,FAIL
@@ -18,4 +18,4 @@ cd /scratch1/04119/pmoolcha/HDM/optimize_cells
 #ibrun gdb --batch -x /scratch1/04119/pmoolcha/HDM/optimize_cells/gdb_script --args python3 -m nested.optimize --config-file-path='config/optimize_DG_MC_excitability_config.yaml' --pop_size=200 --max_iter=2 --path_length=1 --disp --output-dir=data --label=cell1000000 --export
  
 
-ibrun python3 -m nested.optimize --config-file-path='config/optimize_DG_MC_Izhi_leak_spiking_config.yaml' --pop_size=200 --max_iter=50 --path_length=3 --disp --output-dir=data --label=cell1000000
+ibrun python3 -m nested.optimize --config-file-path='config/optimize_DG_MC_Izhi_leak_spiking_config.yaml' --pop_size=200 --max_iter=50 --path_length=3 --disp --output-dir=data --label=cell1039000
